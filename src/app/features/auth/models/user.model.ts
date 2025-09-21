d246530635f8520efdb791ef6aace22df1b10358
EliseLabarrere <lab.elise@outlook.fr>
2025-09-21 20:35:09 +0200

    fix: user.model - Add AuthResponse and RegisterPayload

--- src/app/features/auth/models/user.model.ts
+++ src/app/features/auth/models/user.model.ts
@@ -7,9 +7,27 @@ export interface User {
   isAdmin: boolean;
   secretQuestion: string;
   secretAnswer: string;
-  accounWeward: string;
-  accounInstagram: string;
-  accounDiscord: string;
+  accountWeward?: string;
+  accountInstagram?: string;
+  accountDiscord?: string;
   createdAt: Date;
   updatedAt: Date;
+}
+
+export interface AuthResponse {
+  token: string;
+  user: User;
+}
+
+export interface RegisterPayload {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  secretQuestion: string;
+  secretAnswer: string;
+  accountWeward?: string;
+  accountInstagram?: string;
+  accountDiscord?: string;
 }
