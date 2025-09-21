a691bffc4bb2e7faa6747bf5007e7e2e7c038f50
EliseLabarrere <lab.elise@outlook.fr>
2025-09-21 21:37:12 +0200

    feat: tailwind - add new themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,33 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myBlack: '#131a29',
+        myLightGray: '#f8f7f6',
+        myWhite: '#ffffff',
+        myDarkBlue: '#131a29',
+        myOrange: '#f39359',
+        myTeal: '#53b2aa',
+        myPurple: '#655fe5',
+        myBeige: '#f7ede6',
+
+        primary: "#f39359",
+        "background-light": "#f8f7f6",
+        "background-dark": "#221710"
+      },
+      fontFamily: {
+        display: "Spline Sans"
+      },
+      borderRadius: {
+        DEFAULT: "0.125rem",
+        lg: "0.25rem",
+        xl: "0.5rem",
+        full: "0.75rem"
+      }
+    }
   },
   plugins: [],
 }
