6c66510698020571ab0f8d157f885f624197354f
EliseLabarrere <lab.elise@outlook.fr>
2025-09-25 02:11:04 +0200

    feat: update header routes

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,11 +1,18 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '../../../features/auth/services/auth.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faFilePen,
+  faUserPen,
+  faUser,
+  faRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
@@ -14,6 +21,10 @@ export class HeaderComponent {
   private router = inject(Router);
 
   currentUser = this.authService.currentUser;
+  faFilePen = faFilePen;
+  faUserPen = faUserPen;
+  faUser = faUser;
+  faRightFromBracket = faRightFromBracket;
 
   logout() {
     this.authService.logout();
